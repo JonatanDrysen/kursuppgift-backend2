7f894b8d254bd59a5748d79c885ac0d6427f90b1
JonatanDrysen <jojodrysen@gmail.com>
2022-04-22 17:37:35 +0200

    add routing and some pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import React from "react"
+import { Route, Routes, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage"
+import LoginPage from "../pages/LoginPage"
+import CreateAccountPage from ("../pages/CreateAccountPage")
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/todos" element={ <HomePage /> } />
+        <Route path="/auth/login" element={ <LoginPage /> } />
+        <Route path="/users" element={ <CreateAccountPage /> } />
+      </Routes>
     </div>
   );
 }
